587fd9572b7c67a82fb2a3ef5ed2fa1336a6f996
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2023-02-28 17:15:24 +0200

    update

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,13 @@
+const Contacts = () => {
+    return (
+        <>
+            <div>
+                <h2>Наші контакти</h2>
+                <p>Телефон: (097) 999-77-00</p>
+                <p>Email: connect@gmail.com</p>
+            </div>
+        </>
+    )
+}
+
+export default Contacts;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,30 @@
 import css from '../Header/Header.module.css';
 
+import Contacts from 'components/Contacts/Contacts';
+
+
+import { useState } from "react";
+
 const Header = () => {
-    return (
-    <>
-        <div className={css.header}>
+  const [value, setValue] = useState(false);
+
+  return (
+    <div>
+              <div className={css.header}>
         <h1>"Гуртйся"</h1>
         <ul className={css.menu}>
-            <li className={css.menuItem}>Навчання</li>
-            <li className={css.menuItem}>Форум</li>
-            <li className={css.menuItem}>Про платформу</li>
-            <li className={css.menuItem}>Контакти</li>
-            <li className={css.menuItem}>Вхід\Реєстрація</li>          
+            <li className={css.menuItem}><button className={css.maunuItemBtn}>Навчання</button></li>
+            <li className={css.menuItem}><button className={css.maunuItemBtn}>Форум</button></li>
+            <li className={css.menuItem}><button className={css.maunuItemBtn}>Про платформу</button></li>
+            <li className={css.menuItem}><button className={css.maunuItemBtn} type="button" onClick={() => setValue(true)}>Rjynfynf</button></li>
+            <li className={css.menuItem}><button className={css.maunuItemBtn}>Вхід\Реєстрація</button></li>          
         </ul>    
-        </div>
-    </>
-)
-}
+            </div>
+      
+          {value ? <Contacts /> : ""}
+
+    </div>
+  );
+};  
 
 export default Header;
